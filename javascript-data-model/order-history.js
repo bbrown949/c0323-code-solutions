5503212915d66cc6710b1f91e4f4a3906af7ad02
Bryan Brown <bryanbrown949@gmail.com>
2023-03-21 17:42:27 +0000

    Completed javascript-data-model exercise

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,24 @@
+var orderHistory = [
+  {
+    order: '114-3941689-8772232',
+    delivered: '08/08/2020',
+    item: 'Javascript for impatient programmers'
+  },
+  {
+    order: '113-9984268-1280257',
+    delivered: '07/20/2020',
+    item: 'The Timeless Way Of Building'
+  },
+  {
+    order: '114-2875557-9059409',
+    delivered: '07/07/2020',
+    item: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No lag-Gamecube Adapter'
+  },
+  {
+    order: '113-2883177-2648248',
+    delivered: '07/05/2020',
+    item: 'Gamecube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    item2: 'The Art of Sql'
+  }
+];
+console.log(orderHistory[2].item);
