ca74f0f3c9ab7c305b9a5de35e2b749e5c2c7231
Bryan Brown <bryanbrown949@gmail.com>
2023-04-04 21:04:10 +0000

    javascript-light-bulb - shawn notes

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -3,7 +3,11 @@ const $row = document.querySelector('.row');
 $lightbulb.addEventListener('click', onOff);
 
 function onOff(event) {
-  $lightbulb.className = 'lightbulb off';
-  $row.className = 'row rowoff';
-
+  if ($lightbulb.className === 'lightbulb off') {
+    $lightbulb.className = 'lightbulb on';
+    $row.className = 'row rowon';
+  } else {
+    $lightbulb.className = 'lightbulb off';
+    $row.className = 'row rowoff';
+  }
 }
