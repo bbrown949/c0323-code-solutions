3efdd4ce93500530e7cceb085fdeeaad6b46cb6a
Bryan Brown <bryanbrown949@gmail.com>
2023-05-23 18:38:42 +0000

    updated

--- react-validated-input-component/my-app/src/ValidatedInput.js
+++ react-validated-input-component/my-app/src/ValidatedInput.js
@@ -8,36 +8,32 @@ export default function ValidatedInput() {
   }
 
   let text = '';
-  let checkbox = '';
+  let checkOrX = '';
 
   if (password.length >= 0 && password.length < 8) {
-    text = 'Your password too short.';
-    checkbox = '❌';
+    text = 'Your password is too short.';
+    checkOrX = '❌';
   }
   if (password.length === 0) {
     text = 'A password is required.';
-    checkbox = '❌';
+    checkOrX = '❌';
   } else if (password.length > 7) {
-    checkbox = '✅';
+    checkOrX = '✅';
   }
 
   return (
-    <>
-      <form>
-        <label>Password</label>
-        <div>
-          <input
-            type="password"
-            name="password"
-            onChange={(event) => setPassword(event.target.value)}
-            value={password}
-            onSubmit={handleSubmit}></input>
-          <span class="checkbox" style={{ marginLeft: 10 }}>
-            {checkbox}
-          </span>
-          <p style={{ color: 'red' }}>{text}</p>
-        </div>
-      </form>
-    </>
+    <form>
+      <label>Password</label>
+      <div>
+        <input
+          type="password"
+          name="password"
+          onChange={(event) => setPassword(event.target.value)}
+          value={password}
+          onSubmit={handleSubmit}></input>
+        <span class="checkbox">{checkOrX}</span>
+        <p style={{ color: 'red' }}>{text}</p>
+      </div>
+    </form>
   );
 }
